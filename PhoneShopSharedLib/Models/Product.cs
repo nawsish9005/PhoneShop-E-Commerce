8f1a863a288c1b97578b4c75ad6e06813498a645
nawsish <nawsish2018@gmail.com>
2025-02-20 17:14:13 +0600

    upd

--- PhoneShopSharedLib/Models/Product.cs
+++ PhoneShopSharedLib/Models/Product.cs
@@ -15,8 +15,9 @@ namespace PhoneShopSharedLib.Models
         public decimal Price { get; set; }
         [Required, DisplayName("Product Image")]
         public string? Base64Img { get; set; }
+        [Required, Range(1, 99999)]
         public int Quantity { get; set; }
-        public bool Featured { get; set; }
+        public bool Featured { get; set; } = false;
         public DateTime DateUploaded { get; set; }= DateTime.Now;
     }
 }
